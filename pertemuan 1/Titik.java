b5430bf6b4fd6b3e056947f21c3a3ed476f4ef23
Qun Alfadrian <qunadeian@gmail.com>
2024-02-21 11:44:49 +0700

    pertemuan 1

---
+++ pertemuan 1/Titik.java
@@ -0,0 +1,31 @@
+public class Titik {
+    float absis, ordinat;
+    static int counterTitik;
+
+    Titik(float absisIn, float ordinatIn) {
+        absis = absisIn;
+        ordinat = ordinatIn;
+        counterTitik += 1;
+    }
+
+    public void setAbsis(float absisIn) {
+        absis = absisIn;
+    }
+
+    public void setOrdinat(float ordinatIn) {
+        ordinat = ordinatIn;
+    }
+
+    public float getAbsis() {
+        return absis;
+    }
+
+    public float getOrdinat() {
+        return ordinat;
+    }
+
+    public static int getCounterTitik() {
+        System.out.format("%d", counterTitik);
+        return counterTitik;
+    }
+}
