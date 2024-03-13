55d0e1b6da50fbb0fec176c1ca6c93c559ccc3bc
Qun Alfadrian <qunadeian@gmail.com>
2024-03-13 11:14:21 +0700

    pertemuan 4

--- pertemuan 4/org/BangunDatar/Segitiga.java
+++ pertemuan 4/org/BangunDatar/Segitiga.java
@@ -3,16 +3,20 @@ package org.BangunDatar;
 import org.Poligon.Poligon;
 
 public class Segitiga extends Poligon {
-    private double alas, tinggi;
+    private double sisiA, sisiB, sisiC;
 
-    public Segitiga(double alas, double tinggi, int jumlahSisi) {
-        this.alas = alas;
-        this.tinggi = tinggi;
+    public Segitiga(double sisiA, double sisiB, double sisiC, int jumlahSisi) {
+        this.sisiA = sisiA;
+        this.sisiB = sisiB;
+        this.sisiC = sisiC;
         this.jumlahSisi = jumlahSisi;
     }
 
     public double hitungLuas() {
-        return 0.5*alas*tinggi;
+        double s, luas;
+        s = 0.5*(sisiA+sisiB+sisiC);
+        luas = Math.sqrt(s*(s-sisiA)*(s-sisiB)*(s-sisiC));
+        return luas;
     }
 
     public void printInfo() {
--- pertemuan 4/org/Main/MPoligon.java
+++ pertemuan 4/org/Main/MPoligon.java
@@ -5,12 +5,12 @@ import org.BangunDatar.*;
 public class MPoligon {
     public static void main(String[] args) {
         PersegiPanjang persegi = new PersegiPanjang(10, 10, 4);
-        Segitiga segitiga = new Segitiga(3, 4, 3);
+        Segitiga segitiga2 = new Segitiga(3, 4, 5, 3);
 
         persegi.printInfo();
         System.out.println("Luas Persegi Panjang : " + persegi.hitungLuas());
 
-        segitiga.printInfo();
-        System.out.println("Luas Segitiga : " + segitiga.hitungLuas());
+        segitiga2.printInfo();
+        System.out.println("Luas Segitiga : " + segitiga2.hitungLuas());
     }
 }
